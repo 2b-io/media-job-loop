78dcda16b2e08ce0c39ba6c953393f3aa30e86c8
LongLH <d@dapps.me>
2018-11-30 10:21:12 +0700

    remove unused config

--- src/infrastructure/config.js
+++ src/infrastructure/config.js
@@ -1,11 +1,5 @@
 export default {
   apiUrl: process.env.API_URL,
-  migrateSever: process.env.MIGRATE_SEVER,
-  server: {
-    base: process.env.BASE_URL,
-    bind: process.env.SERVER_BIND,
-    port: process.env.SERVER_PORT
-  },
   amq: {
     host: process.env.AMQ_HOST,
     queue: process.env.AMQ_QUEUE,
