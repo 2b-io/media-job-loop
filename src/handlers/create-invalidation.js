cf2cbc02019f6c8285dab9460aff2608fb526fe0
cuongtv <truongcuong90@gmail.com>
2018-12-06 17:12:54 +0700

    - Refactor invalidation

---
+++ src/handlers/create-invalidation.js
@@ -0,0 +1,62 @@
+import ms from 'ms'
+
+import api from 'services/api'
+import {
+  invalidationPatternService,
+  invalidationProjectService,
+  invalidationPresetService
+} from 'services/invalidation'
+
+const invalidationPatterns = async (projectIdentifier, invalidationIdentifier) => {
+  const {
+    Id: invalidationId,
+    Status: status
+  } = await invalidationPatternService(projectIdentifier, invalidationIdentifier)
+
+  if (!invalidationId) {
+    return null
+  }
+
+   await api.call(
+     'patch',
+     `/projects/${ projectIdentifier }/invalidations/${ invalidationIdentifier }`,
+     { cdnInvalidationRef: invalidationId }
+   )
+
+  return {
+    name: 'CHECK_INVALIDATION',
+    when: Date.now(),
+    payload: {
+      projectIdentifier,
+      invalidationIdentifier
+    }
+  }
+}
+
+const invalidationProject = async (projectIdentifier) => {
+  return await invalidationProjectService(projectIdentifier)
+}
+
+export default async (job) => {
+  const {
+    payload: {
+      invalidationType,
+      projectIdentifier,
+      invalidationIdentifier
+    }
+  } = job
+
+  if (!invalidationType) {
+    return null
+  }
+
+  if (type === 'PRESET') {
+    return await invalidationPreset()
+  }
+
+  if (type === 'PATTERNS') {
+    return await invalidationPatterns(projectIdentifier, invalidationIdentifier)
+  }
+
+  return await invalidationProject(projectIdentifier)
+}
--- src/services/cache/index.js
+++
@@ -1,2 +0,0 @@
-export { default as invalidateByPatterns } from './invalidate-by-patterns'
-export { default as invalidateByProject } from './invalidate-by-project'
