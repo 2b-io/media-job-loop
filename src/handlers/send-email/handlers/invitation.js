a9494abe0c132f8d2b96eade9d38fd1038b42822
cuongtv <truongcuong90@gmail.com>
2018-12-14 15:51:31 +0700

    - job loop send email invitation

--- src/handlers/send-email/handlers/invitation.js
+++ src/handlers/send-email/handlers/invitation.js
@@ -1,9 +1,22 @@
+import config from 'infrastructure/config'
+import api from 'services/api'
+
 export default async (job) => {
+  const { accountIdentifier, inviterName, message } = job.payload
+
+  const account = await api.call('get', `/accounts/${ accountIdentifier }`)
+
+  const resetToken = await api.call('post', '/reset-tokens', { email: account.email } )
+
   return {
     template: 'invitation',
-    to: 'user@example.com',
+    to: config.sendgrid.sender,
+    receivers: account.email,
     locals: {
-
+      email: account.email,
+      inviterName,
+      message,
+      activateLink: `${ config.serverBind }:${ config.serverPort }/reset-password/${ resetToken.token }`
     }
   }
 }
--- src/handlers/send-email/handlers/password-recovery.js
+++ src/handlers/send-email/handlers/password-recovery.js
@@ -1,9 +1,18 @@
+import config from 'infrastructure/config'
+import api from 'services/api'
+
 export default async (job) => {
+  const { accountIdentifier, token } = job.payload
+  console.log(' accountIdentifier, token',  accountIdentifier, token)
+  const account = await api.call('get', `/accounts/${ accountIdentifier }`)
+
   return {
     template: 'password-recovery',
-    to: 'user@example.com',
+    to: config.sendgrid.sender,
+    receivers: account.email,
     locals: {
-
+      name: account.name,
+      resetLink: `${ config.serverBind }:${ config.serverPort }/reset-password/${ token }`
     }
   }
 }
