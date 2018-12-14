1e6ad8984859733a420d95b3913236b9a37ba576
cuongtv <truongcuong90@gmail.com>
2018-12-14 16:58:26 +0700

    - Remove check isDeleted

--- src/handlers/create-infrastructure.js
+++ src/handlers/create-infrastructure.js
@@ -14,9 +14,6 @@ export default async (job) => {
   // get project
   const project = await api.call('get', `/projects/${ projectIdentifier }`)
 
-  if (!project || project.isDeleted) {
-    return null
-  }
   // get infrastructure
   const infrastructure = await api.call('get', `/projects/${ projectIdentifier }/infrastructure`)
 
