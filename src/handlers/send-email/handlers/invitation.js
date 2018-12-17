b4225fb267984fddbec6d5a3a3111de041cc4553
cuongtv <truongcuong90@gmail.com>
2018-12-17 11:59:10 +0700

    - Fix duplicate send email when create account and recovery password

--- src/handlers/send-email/handlers/invitation.js
+++ src/handlers/send-email/handlers/invitation.js
@@ -2,9 +2,8 @@ import config from 'infrastructure/config'
 import api from 'services/api'
 
 export default async (job) => {
-  const { accountIdentifier, inviterName, message } = job.payload
+  const { accountIdentifier, token, inviterName, message } = job.payload
   const account = await api.call('get', `/accounts/${ accountIdentifier }`)
-  const resetToken = await api.call('post', '/reset-tokens', { email: account.email } )
 
   return {
     template: 'invitation',
@@ -14,7 +13,7 @@ export default async (job) => {
       email: account.email,
       inviterName,
       message,
-      activateLink: `${ config.serverBind }:${ config.serverPort }/reset-password/${ resetToken.token }`
+      activateLink: `${ config.serverBind }:${ config.serverPort }/reset-password/${ token }`
     }
   }
 }
--- src/handlers/send-email/handlers/welcome.js
+++ src/handlers/send-email/handlers/welcome.js
@@ -2,9 +2,8 @@ import config from 'infrastructure/config'
 import api from 'services/api'
 
 export default async (job) => {
-  const { accountIdentifier } = job.payload
+  const { accountIdentifier, token } = job.payload
   const account = await api.call('get', `/accounts/${ accountIdentifier }`)
-  const resetToken = await api.call('post', '/reset-tokens', { email: account.email } )
 
   return {
     template: 'welcome',
@@ -12,7 +11,7 @@ export default async (job) => {
     receivers: email,
     locals: {
       email,
-      activateLink: `${ config.serverBind }:${ config.serverPort }/reset-password/${ resetToken.token }`
+      activateLink: `${ config.serverBind }:${ config.serverPort }/reset-password/${ token }`
     }
   }
 }
