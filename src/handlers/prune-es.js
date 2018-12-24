ab35fc0f1ac7a3d5925a883ade2025edecbafb3a
cuongtv <truongcuong90@gmail.com>
2018-12-24 18:25:41 +0700

    - Prune es

---
+++ src/handlers/prune-es.js
@@ -0,0 +1,46 @@
+import ms from 'ms'
+
+import api from 'services/api'
+
+export default async (job) => {
+  console.log('PRUNE_ES...')
+
+  const {
+    name,
+    when,
+    payload: {
+      projectIdentifier,
+      lastSynchronized,
+      maxKeys
+    }
+  } = job
+
+  const { isTruncated } = await api.call(
+    'delete',
+    `/projects/${ projectIdentifier }/files`,
+    { lastSynchronized: new Date(lastSynchronized).toISOString(), maxKeys }
+  )
+
+  if (!isTruncated) {
+    return {
+      name,
+      when: Date.now(),
+      payload: {
+        projectIdentifier,
+        maxKeys,
+        lastSynchronized
+      }
+    }
+  }
+
+  console.log('PRUNE_ES_SUCCESS')
+
+  return {
+    name: 'SYNC_S3_TO_ES',
+    when: Date.now() + ms('3d'),
+    payload: {
+      projectIdentifier,
+      maxKeys
+    }
+  }
+}
