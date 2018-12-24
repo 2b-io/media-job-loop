901db5e44ae0faa27a7815f746ea35151538ada5
cuongtv <truongcuong90@gmail.com>
2018-12-24 19:51:14 +0700

    - Fix b

--- src/services/s3.js
+++ src/services/s3.js
@@ -3,37 +3,33 @@ import s3 from 'infrastructure/s3'
 
 const MAX_KEYS = 1000
 
-const delete = async (keys) => {
-  let keyFrom = 0
-  do {
-    const subKeys = keys.slice(keyFrom, keyFrom + MAX_KEYS)
-    await s3.deleteObjects({
-      Bucket: s3.config.bucket,
-      Delete: {
-        Objects: subKeys.map(({ key }) => ({ Key: key }))
-      }
-    }).promise()
 
-    keyFrom = keyFrom + subKeys.length
-  } while (keyFrom < keys.length)
-}
-
-const head = async (key) => {
-  return await s3.headObject({
-    Bucket: config.aws.s3.bucket,
-    Key: key
-  }).promise()
-}
-
-const list = async ({ params }) => {
-  return await s3.listObjectsV2({
-    Bucket: config.aws.s3.bucket,
-    ...params
-  }).promise()
-}
 
 export default {
-  delete,
-  head,
-  list
+  async delete (keys) {
+    let keyFrom = 0
+    do {
+      const subKeys = keys.slice(keyFrom, keyFrom + MAX_KEYS)
+      await s3.deleteObjects({
+        Bucket: s3.config.bucket,
+        Delete: {
+          Objects: subKeys.map(({ key }) => ({ Key: key }))
+        }
+      }).promise()
+
+      keyFrom = keyFrom + subKeys.length
+    } while (keyFrom < keys.length)
+  },
+  async head (key) {
+    return await s3.headObject({
+      Bucket: config.aws.s3.bucket,
+      Key: key
+    }).promise()
+  },
+  async list ({ params }) {
+    return await s3.listObjectsV2({
+      Bucket: config.aws.s3.bucket,
+      ...params
+    }).promise()
+  }
 }
